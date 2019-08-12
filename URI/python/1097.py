e26e14b262f9a848d0bf4dc495d2192a76bced93
Carlos Eduardo <0800@cardu.dev>
2019-08-12 15:41:11 -0300

    Add solution to URI 1097 in python

--- URI/python/1097.py
+++ URI/python/1097.py
@@ -1,3 +1,6 @@
+j = 7
 for i in range(1,11,2):
-    for j in reversed(range(5, 8)):
         print('I={0} J={1}'.format(i,j))
+        print('I={0} J={1}'.format(i,j-1))
+        print('I={0} J={1}'.format(i,j-2))
+        j += 2
