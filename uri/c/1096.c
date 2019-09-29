bce9813f98ca463219b6fdae92425c1a7837951b
Carlos Eduardo <0800@cardu.dev>
2019-09-29 17:17:28 -0300

    Add solution to URI 1096 - C

---
+++ uri/c/1096.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 1;
+
+    for (i; i <= 9; i += 2) {
+        printf("I=%d J=7\n", i);
+        printf("I=%d J=6\n", i);
+        printf("I=%d J=5\n", i);
+    } 
+
+    return 0;
+
+}
