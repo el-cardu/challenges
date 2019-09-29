24a51ccfd0ac5c4318177448594f5f58dec62751
Carlos Eduardo <0800@cardu.dev>
2019-09-29 11:38:32 -0300

    Add solution to URI 105 - C

---
+++ uri/c/1059.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    for (n == 2; n <= 100; n++) {
+        if (n % 2 == 0 && n != 0) {
+            printf("%d\n", n);
+        }
+    }
+
+    return 0;
+
+}
