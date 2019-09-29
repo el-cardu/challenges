21f6848cc30e91d598c507ae08b6c1e3a7d98cc7
Carlos Eduardo <0800@cardu.dev>
2019-09-29 17:24:45 -0300

    Add solution to URI 1097 - C

---
+++ uri/c/1097.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 1, j = 7;
+
+    for (i; i <= 9; i += 2) {
+        printf("I=%d J=%d\n", i, j);
+        printf("I=%d J=%d\n", i, j-1);
+        printf("I=%d J=%d\n", i, j-2);
+        j += 2;
+    }
+
+    return 0;
+
+}
