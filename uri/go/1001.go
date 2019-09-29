7c25f35eeecbe1ef96b2765a4e8d3dfada5f1ef4
Carlos Eduardo <0800@cardu.dev>
2019-09-29 02:13:27 -0300

    Add solution to URI 1001 - go

---
+++ uri/go/1001.go
@@ -0,0 +1,16 @@
+package main
+ 
+import (
+    "fmt"
+)
+ 
+func main() {
+
+    var A int
+    var B int
+
+    fmt.Scanf("%d", &A)
+    fmt.Scanf("%d", &B)
+    fmt.Printf("X = %d\n", A + B)
+
+}
