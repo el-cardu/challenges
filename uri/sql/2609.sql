3493d257fdf7ee17342fdcb7bac4f838218b696e
Carlos Eduardo <0800@cardu.dev>
2019-10-02 22:00:34 -0300

    Add solutions in sql

---
+++ uri/sql/2609.sql
@@ -0,0 +1,3 @@
+SELECT categories.name, SUM(amount)
+FROM products INNER JOIN categories ON products.id_categories = categories.id
+GROUP BY categories.name;
---
+++ uri/sql/2614.sql
@@ -0,0 +1,4 @@
+SELECT customers.name, rentals.rentals_date
+FROM customers INNER JOIN rentals ON customers.id = rentals.id_customers
+WHERE EXTRACT(YEAR FROM rentals_date) = 2016
+AND EXTRACT(MONTH FROM rentals_date) = 9;
