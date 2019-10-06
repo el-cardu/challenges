fe5bb689ae5d623752d7243c8a1bed599eac7274
Carlos Eduardo <0800@cardu.dev>
2019-10-06 15:32:47 -0300

    Add solution to URI 1046 - C

---
+++ uri/c/1046.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int inicio, fim;
+    scanf("%d %d", &inicio, &fim);
+
+    if (fim <= inicio) {
+        fim += 24;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", (fim - inicio));
+
+    return 0;
+
+}
