56d9cf0024e5cc7a2fee10e7c8c5ff031d29f578
Carlos Eduardo <0800@cardu.dev>
2019-10-06 13:01:22 -0300

    Add solution to URI 2344 - C

---
+++ uri/c/2344.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int nota;
+    scanf("%d", &nota);
+
+    if (nota >= 86 && nota <= 100) {
+        printf("A\n");
+    } else if (nota >= 61 && nota <= 85) {
+        printf("B\n");
+    } else if (nota >= 36 && nota <= 60) {
+        printf("C\n");
+    } else if (nota >= 1 && nota <= 35) {
+        printf("D\n");
+    } else if (nota == 0) {
+        printf("E\n");
+    }
+
+
+    return 0;
+
+}
