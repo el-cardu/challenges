ab64002d618825387caa962114518b5aec86cb3d
Carlos Eduardo <0800@cardu.dev>
2019-10-06 13:01:32 -0300

    Add solution to URI 2483 - C

---
+++ uri/c/2483.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+    
+    printf("Feliz nat");
+
+    while (N > 0) {
+        printf("a");
+        N--;
+    }
+
+    printf("l!\n");
+
+    return 0;
+
+}
