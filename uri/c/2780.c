dcdd88f8bb41b7492943824e2f2201318e88c218
Carlos Eduardo <0800@cardu.dev>
2019-10-23 12:26:54 -0300

    Add solution to URI - 2780 - C

---
+++ uri/c/2780.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int D;
+    scanf("%d", &D);
+
+    if (D <= 800) {
+        printf("1\n");
+    } else if (800 < D && D <= 1400) {
+        printf("2\n");
+    } else if (1400 < D && D <= 2000) {
+        printf("3\n");
+    }
+
+    return 0;
+
+}
