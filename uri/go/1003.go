b843bc268893ac6df80f22768d3fbc50265ceb02
Cardu <cardu@e-moving.com.br>
2020-04-05 20:46:44 -0300

    Add Go solution to URI 1003

---
+++ uri/go/1003.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var A int
+	var B int
+	var soma int
+
+	fmt.Scanf("%d", &A)
+	fmt.Scanf("%d", &B)
+
+	soma = A + B
+
+	fmt.Printf("SOMA = %d\n", soma)
+
+}
