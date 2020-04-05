044a1386434b1505a780a4bce2987a69d87ff3a3
Cardu <cardu@e-moving.com.br>
2020-04-05 20:50:00 -0300

    Add Go solution to URI 1004

---
+++ uri/go/1004.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var A int
+	var B int
+
+	fmt.Scanf("%d", &A)
+	fmt.Scanf("%d", &B)
+
+	prod := A * B
+
+	fmt.Printf("PROD = %d\n", prod)
+
+
+
+}
